37a99970cae9a9170eddd0c35f85560320b504b2
sonya brazell <92110758+sonyabrazell@users.noreply.github.com>
2021-11-15 07:37:08 -0600

    Modify components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css'
 import TitleBar from './TitleBar/TitleBar';
 
+
 class App extends Component {
     constructor(props){
         super(props);
@@ -42,21 +43,6 @@ class App extends Component {
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="row">
-                    <div className="col-md-4">
-                        {/*Button here to move to the previous book viewed*/}
-                        <button onClick={this.goToPreviousBook}>Previous Book</button>
-                    </div>
-                    <div className="col-md-4">
-                        {/*Display book with cover here*/}
-                        <h1>{this.books[this.state.bookNumber].title}</h1>
-                        <h4>{this.books[this.state.bookNumber].author}</h4>
-                    </div>
-                    <div className="col-md-4">
-                    {/*Button here to move to the next book viewed*/}
-                    <button onClick={this.goToNextBook}>Next Book</button>
-                    </div>
-                </div>
             </div>
         )
     }
